56dd6a825edeac019ce72989d71f89750144fb0c
Jamesmbaya <chinkupozee@gmail.com>
2025-01-31 07:08:55 -0800

    new file and modify

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,3 +1,53 @@
+import React, { Component } from "react";
+import "./App.css";
+import ParentComponent from "./components/ParentComponent";
+// import Greet from "./components/Greet";
+// import FunctionClick from "./components/FunctionClick";
+// import ClassClick from "./components/ClassClick";
+// import EventBind from "./components/EventBind";
+// import Counter from "./components/Greets";
+// import Welcome from "./components/Welcome";
+// import Hello from "./components/Hello";
+// import Message from "./components/Message";
+// import Counter from "./components/Counter";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ParentComponent/>
+        {/* <EventBind/> */}
+        {/* <FunctionClick/> */}
+        {/* <ClassClick/> */}
+        {/* <Greet/> */}
+        {/* <Counter/> */}
+        {/* <Message /> */}
+        {/* <Greet name="James" heroName="Batman">
+          <p>This is chidren props</p>
+        </Greet> */}
+        {/* <Greet name="Mbaya" heroName="Superman">
+          <button>Action</button>
+        </Greet> */}
+        {/* <Greet name="Muthiora" heroName="Wonder Woman" /> */}
+        {/* <Welcome name="James" heroName="Batman" /> */}
+        {/* <Welcome name="Mbaya" heroName="Superman" /> */}
+        {/* <Welcome name="Muthiora" heroName="Wonder Woman" />  */}
+        {/* <Welcome/>
+//       <Hello/> */}
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
+
+
+
+
+
+
 // import './App.css';
 // import Greet from './components/Greet';
 // import Welcome from './components/Welcome';
@@ -18,40 +68,3 @@
 // }
 
 // export default App;
-
-import React, { Component } from "react";
-import "./App.css";
-// import Greet from "./components/Greet";
-import FunctionClick from "./components/FunctionClick";
-// import Counter from "./components/Greets";
-// import Welcome from "./components/Welcome";
-// import Hello from "./components/Hello";
-// import Message from "./components/Message";
-// import Counter from "./components/Counter";
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        {/* <Greet/> */}
-        {/* <Counter/> */}
-        {/* <Message /> */}
-        {/* <Greet name="James" heroName="Batman">
-          <p>This is chidren props</p>
-        </Greet> */}
-        {/* <Greet name="Mbaya" heroName="Superman">
-          <button>Action</button>
-        </Greet> */}
-        <FunctionClick/>
-        {/* <Greet name="Muthiora" heroName="Wonder Woman" /> */}
-        {/* <Welcome name="James" heroName="Batman" /> */}
-        {/* <Welcome name="Mbaya" heroName="Superman" /> */}
-        {/* <Welcome name="Muthiora" heroName="Wonder Woman" />  */}
-        {/* <Welcome/>
-//       <Hello/> */}
-      </div>
-    );
-  }
-}
-
-export default App;
--- hello-world/src/components/FunctionClick.js
+++ hello-world/src/components/FunctionClick.js
@@ -3,11 +3,11 @@ import React from 'react'
 function FunctionClick() {
 
     function clickHandler(){
-        console.log("Button clicked")
+        console.log('Button clicked')
     }
   return (
     <div>
-        <button onClick={clickHandler}>CLICK</button>
+        <button onClick={clickHandler()}>CLICK</button>
     </div>
   )
 }
