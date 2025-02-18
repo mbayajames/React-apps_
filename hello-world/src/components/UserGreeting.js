60037be589160d69408d94c47f311e9c35faaf64
Jamesmbaya <chinkupozee@gmail.com>
2025-02-18 06:14:05 -0800

    feat: add UserGreeting component to display personalized messages

---
+++ hello-world/src/components/UserGreeting.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class UserGreeting extends Component {
+
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             
+        }
+    }
+    
+  render() {
+    return (
+      <div>UserGreeting</div>
+    )
+  }
+}
+
+export default UserGreeting;
