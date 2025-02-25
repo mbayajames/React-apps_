c583c1cdc80058750f816fb48a7aa4c3a621011d
Jamesmbaya <chinkupozee@gmail.com>
2025-02-25 01:16:40 -0800

    Update SignUp.js to enhance user registration flow and UI improvements

---
+++ forms/src/components/SignUp.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const SignUp = () => {
+  return (
+    <div>
+      <h2>Sign Up</h2>
+      <form>
+        <label>
+          Email:
+          <input type="email" name="email" />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input type="password" name="password" />
+        </label>
+        <br />
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
