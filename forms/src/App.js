82e02a2b7b28769b9cb98e433aa7b0cd112da09b
Jamesmbaya <chinkupozee@gmail.com>
2025-02-25 01:15:11 -0800

    Update App.js to enhance functionality and improve component structure

--- forms/src/App.js
+++ forms/src/App.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from 'react';
+import SignUp from './components/SignUp';
+import SignIn from './components/SignIn';
+import StudentForm from './components/StudentForm';
+import './App.css';
 
 function App() {
   return (
-    <div>App</div>
+    <div className="App">
+      <h1>Welcome to the Form Project</h1>
+      <SignUp/>
+      <SignIn/>
+      <StudentForm/>
+    </div>
   )
 }
 
