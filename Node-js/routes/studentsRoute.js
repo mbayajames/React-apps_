13265dd54d561a3c22d4d087d3c534bfba90df4c
Jamesmbaya <chinkupozee@gmail.com>
2025-02-24 22:39:32 -0800

    Add Node-js/routes/studentsRoute.js for student-related API routes

--- Node-js/routes/studentsRoute.js
+++ Node-js/routes/studentsRoute.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const routes = express.Router();
 
+// get all students
 routes.get('/students', (req,res)=>{
     res.send({type:'Get Request'});
 });
 
+// Add student
 routes.post('/students', (req,res)=>{
     res.send({type:'Post Request'});
 });
 
+// get student
 routes.put('/students/:id', (req,res)=>{
     res.send({type:'Update Request'});
 });
 
+// update student
 routes.delete('/students:/id', (req,res)=>{
     res.send({type:'Delete Request'});
 });
