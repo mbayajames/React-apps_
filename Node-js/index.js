408a20940502b98c78514ee51132343e45e07c44
Jamesmbaya <chinkupozee@gmail.com>
2025-02-28 12:32:12 -0800

    Update index.js to improve error handling and routing

--- Node-js/index.js
+++ Node-js/index.js
@@ -1,12 +1,30 @@
 const express = require('express');
-const routes = require('./routes/studentsRoute');
+const studentsroutes = require('./routes/studentsRoute');
 const app = express();
 require('dotenv').config();
 require('./helpers/init_mongodb');
 
+app.use(express.json());
+app.use("/students",studentsroutes )
+
+
+//handling errors
+
+app.use(async (req, res, next) =>{
+    next(createError.NotFound());
+});
+
+//Error handler
+app.use((err,req, res, next) =>{
+    res.status(err.status || 500);
+    res.send({
+        error: {
+            status: err.status || 500,
+            message:err.message,
+        },
+    });
+});
+
 app.listen(process.env.port || 4000 , function(){
     console.log('Now Listening for request on:http://localhost:4000')
 });
-
-
-app.use(routes);
