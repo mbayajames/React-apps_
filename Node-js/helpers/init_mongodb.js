05c5a06f0c0da1a3eb3ae87ef55070def09b5286
mbayajames <jmuthiora244@gmail.com>
2025-03-06 23:52:19 -0800

    Remove init_mongodb.js as part of database configuration cleanup

--- Node-js/helpers/init_mongodb.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.connect(process.env.MONGODB_URI, {dbName: process.env.DB_NAME})
-.then(()=>{
-    console.log('mongodb connected')
-})
-
-.catch((err)=>console.log(err.message));
