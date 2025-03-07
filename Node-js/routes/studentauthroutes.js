7788badddd37cfb81cebd94556f78c0b7cae53b3
mbayajames <jmuthiora244@gmail.com>
2025-03-07 00:11:30 -0800

    Remove student authentication routes as part of project restructuring

--- Node-js/routes/studentauthroutes.js
+++
@@ -1,9 +0,0 @@
-const studentAuthController = require('../Controllers/studentauthcontroller')
-const express = require('express')
-
-
-const studentAuthRoute = express.Router()
-
-studentAuthRoute.post('/studentauthadd', studentAuthController.register)
-
-module.exports = studentAuthRoute
